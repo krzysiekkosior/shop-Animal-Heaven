a920debc2c2fa608267f26e4ff3df48235509051
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2020-10-12 13:03:51 +0200

    dodany plik .js do wyświetlania ikonek, dodany context_processor z datą

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'shop_app.context_processor.get_date'
             ],
         },
     },
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl'
 
 TIME_ZONE = 'UTC'
 
@@ -123,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
---
+++ shop/shop_app/context_processor.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+def get_date(request):
+    return {'date': datetime.today()}
