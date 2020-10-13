b898271cfdd0f485b19f1d0bf677577023522b92
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2020-10-13 20:05:52 +0200

    testy dla widoków marek

--- shop/shop_app/tests.py
+++ shop/shop_app/tests.py
@@ -1,8 +1,6 @@
-from django.test import TestCase
 from django.urls import reverse_lazy
 import pytest
-
-from shop_app.models import Category
+from shop_app.models import Category, Brand
 
 
 @pytest.mark.django_db
@@ -88,3 +86,57 @@ def test_brands_list_url(client):
 def test_brand_details_url(client, brand):
     response = client.get(f'/brand/{brand.id}/')
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_add_brand_as_admin(client, admin_perms):
+    client.login(username='admin', password='admin1')
+    response = client.get(reverse_lazy('add_brand'))
+    assert response.status_code == 200
+
+    client.post(reverse_lazy('add_brand'), {'name': 'test', 'creation_year': 2020})
+    assert Brand.objects.all().count() == 1
+    assert Brand.objects.first().name == 'test'
+
+
+@pytest.mark.django_db
+def test_add_brand_as_client(client, client_perms):
+    client.login(username='user', password='user1')
+    response = client.get(reverse_lazy('add_brand'))
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_edit_brand_as_admin(client, brand, admin_perms):
+    client.login(username='admin', password='admin1')
+    response = client.get(f'/brand/edit/{brand.id}/')
+    assert response.status_code == 200
+
+    client.post(f'/brand/edit/{brand.id}/', {'name': f'{brand.name}', 'creation_year': 2020})
+    assert Brand.objects.first().creation_year == 2020
+
+
+@pytest.mark.django_db
+def test_edit_brand_as_client(client, brand, client_perms):
+    client.login(username='user', password='user1')
+    response = client.get(reverse_lazy('add_brand'))
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_delete_brand_as_admin(client, brand, admin_perms):
+    client.login(username='admin', password='admin1')
+    response = client.get(f'/brand/delete/{brand.id}/')
+    assert response.status_code == 200
+
+    client.post(f'/brand/delete/{brand.id}/', {'action': ''})
+    assert Brand.objects.count() == 1
+
+    client.post(f'/brand/delete/{brand.id}/', {'action': 'delete'})
+    assert Brand.objects.count() == 0
+
+@pytest.mark.django_db
+def test_delete_brand_as_client(client, brand, client_perms):
+    client.login(username='user', password='user1')
+    response = client.get(f'/brand/delete/{brand.id}/')
+    assert response.status_code == 403
