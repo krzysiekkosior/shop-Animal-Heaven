65761e7b81d1d5fa5709703b8828565027eb0372
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2020-10-13 12:03:04 +0200

    tworzenie konta

--- shop/accounts/views.py
+++ shop/accounts/views.py
@@ -1,5 +1,15 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# class LoginView(LoginView):
-#     pass
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = '/'
+    template_name = 'form.html'
+
+    def form_valid(self, form):
+        response = super(SignUpView, self).form_valid(form)
+        login(self.request, self.object)
+        return response
